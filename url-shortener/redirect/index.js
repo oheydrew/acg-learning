794c30397a33d14395672af6f945346aeabec0da
Drew Noll <andrew.noll@acloud.guru>
2020-01-02 17:31:45 +1100

    :art: Add dynamoDb resource, fix some bugs, untrack Resources folder changes

--- url-shortener/redirect/index.js
+++ url-shortener/redirect/index.js
@@ -1,20 +1,50 @@
 'use strict';
 
-const handler = (event, _context, callback) => {
-  console.log(JSON.stringify(event));
-  const slug = event.pathParameters.slug;
-  const target =
-    process.env[`URL_${slug.toUpperCase()}`] ||
-    'http://serverless.com/framework/docs';
+const AWS = require('aws-sdk');
 
-  callback(null, {
-    statusCode: 302,
-    body: target,
-    headers: {
-      Location: target,
-      'Content-Type': 'text/plain'
+const TABLE_NAME = `${process.env.SLS_STAGE}-shortened-urls`;
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const handler = (event, _context, handlerCallback) => {
+  console.log(JSON.stringify(event));
+
+  const slug = event.pathParameters.slug;
+
+  const getQuery = {
+    TableName: TABLE_NAME,
+    Key: {
+      slug: slug
     }
-  });
+  };
+
+  const getCallback = handlerCallback => (err, data) => {
+    if (err) {
+      console.log(`ERROR: ${err}`);
+      return handlerCallback(err);
+    }
+
+    if (data.Item) {
+      handlerCallback(null, {
+        statusCode: 302,
+        body: data.Item.long_url,
+        headers: {
+          Location: data.Item.long_url,
+          'Content-Type': 'text/plain'
+        }
+      });
+    } else {
+      handlerCallback(null, {
+        statusCode: 404,
+        body:
+          "This shortened link doesn't exist, check that you entered it right.",
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+    }
+  };
+
+  documentClient.get(getQuery, getCallback(handlerCallback));
 };
 
 module.exports = { handler };
