1b0d3e46806cd8d3ee76eb7807e5710883ed3d4f
Drew Noll <andrew.noll@acloud.guru>
2020-01-03 10:14:34 +1100

    :art: Promises! Also, more gitignore

--- url-shortener/redirect/index.js
+++ url-shortener/redirect/index.js
@@ -5,6 +5,23 @@ const AWS = require('aws-sdk');
 const TABLE_NAME = `${process.env.SLS_STAGE}-shortened-urls`;
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const successMessage = item => ({
+  statusCode: 302,
+  body: item.long_url,
+  headers: {
+    Location: item.long_url,
+    'Content-Type': 'text/plain'
+  }
+});
+
+const failMessage = () => ({
+  statusCode: 404,
+  body: "This shortened link doesn't exist, check that you entered it right.",
+  headers: {
+    'Content-Type': 'text/plain'
+  }
+});
+
 const handler = (event, _context, handlerCallback) => {
   console.log(JSON.stringify(event));
 
@@ -17,34 +34,19 @@ const handler = (event, _context, handlerCallback) => {
     }
   };
 
-  const getCallback = handlerCallback => (err, data) => {
-    if (err) {
+  documentClient
+    .get(getQuery)
+    .promise()
+    .then(data => {
+      console.log({ data });
+      data.Item
+        ? handlerCallback(null, successMessage(data.Item))
+        : handlerCallback(null, failMessage());
+    })
+    .catch(err => {
       console.log(`ERROR: ${err}`);
       return handlerCallback(err);
-    }
-
-    if (data.Item) {
-      handlerCallback(null, {
-        statusCode: 302,
-        body: data.Item.long_url,
-        headers: {
-          Location: data.Item.long_url,
-          'Content-Type': 'text/plain'
-        }
-      });
-    } else {
-      handlerCallback(null, {
-        statusCode: 404,
-        body:
-          "This shortened link doesn't exist, check that you entered it right.",
-        headers: {
-          'Content-Type': 'text/plain'
-        }
-      });
-    }
-  };
-
-  documentClient.get(getQuery, getCallback(handlerCallback));
+    });
 };
 
 module.exports = { handler };
