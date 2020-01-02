80b3cf82564acdb085e1710b234d623314f1ede8
Drew Noll <andrew.noll@acloud.guru>
2020-01-02 15:11:29 +1100

    :sparkles: Add URL shortener POST referrer details

--- url-shortener/create/index.js
+++ url-shortener/create/index.js
@@ -1,7 +1,7 @@
 'use strict';
-const URL_PREFIX = 'mysite.com';
+const QueryString = require('querystring');
 
-const htmlPage = prefix => `
+const htmlPage = (prefix, submittedUrl) => `
   <html>
     <head>
       <meta charset="UTF-8">
@@ -10,17 +10,26 @@ const htmlPage = prefix => `
 
     <body>
       <h1>✂️ 🎉 🎊 ✂️</h1>
+      <h3>Original URL: ${submittedUrl}</h3>
       <h3>Your snipped URL: <a href="https://${prefix}/fake">${prefix}/fake</a></h3>
     </body>
   </html>
 `;
 
 const handler = (event, context, callback) => {
+  const {
+    headers: { referrer },
+    body
+  } = event;
+
+  const { link: submittedUrl } = QueryString.parse(body);
+
   console.log(JSON.stringify(event));
+  console.log({ submittedUrl });
 
   callback(null, {
     statusCode: 200,
-    body: htmlPage(URL_PREFIX),
+    body: htmlPage(referrer, submittedUrl),
     headers: { 'Content-Type': 'text/html' }
   });
 };
